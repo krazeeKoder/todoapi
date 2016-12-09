01f0be86f3dc4c8b2d0a1e00d647f1477e2a9e2c
KrazeeKoder <tony.tulai@gmail.com>
2016-12-09 08:28:43 -0800

    small refactor

--- server.js
+++ server.js
@@ -9,20 +9,6 @@ var todoNextId = 1;
 
 app.use(bodyParser.json());
 
-// var todos = [{
-// 	id: 1,
-// 	description: 'Meet mom for lunch',
-// 	completed: false
-// }, {
-// 	id: 2,
-// 	description: 'Go to market',
-// 	completed: false
-// }, {
-// 	id: 3,
-// 	description: 'Make doctors appointment',
-// 	completed: true
-// }];
-
 app.get('/', function(req, res) {
 	res.send('Todo API Root');
 });
@@ -74,6 +60,19 @@ app.post('/todos', function (req, res) {
 	//console.log('description ' + body.description);
 	res.json(body);
 });
+
+app.delete('/todos/:id', function(req,res){
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos,{id:todoId});
+
+	if (!matchedTodo) {
+	     res.status(404).send();
+	} else{
+		todos = _.without(todos,matchedTodo);
+	}
+	res.json(matchedTodo);
+});
+
 
 
 app.listen (PORT, function() {
