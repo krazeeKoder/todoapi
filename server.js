76a1a1386382dfe50b18665ef297e3b3fbea28ce
KrazeeKoder <tony.tulai@gmail.com>
2016-12-08 21:08:18 -0800

    added underscore library and whitelisted inputs

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -34,13 +36,8 @@ app.get('/todos', function (req, res) {
 //GET /todos/:id
 app.get('/todos/:id', function(req, res){
 	var todoId = parseInt(req.params.id, 10);
-	//var matchedTodo;
+	var matchedTodo = _.findWhere(todos,{id: todoId});
 
-	// todos.forEach(function(todo){
-	// 	if (todo.id === todoId) {
-	// 		matchedTodo = todo;
-	// 	}
-	// });
 
 	// if (matchedTodo) { 
 	// 	res.json(matchedTodo);	
@@ -62,7 +59,14 @@ app.get('/todos/:id', function(req, res){
 // POST /todos
 
 app.post('/todos', function (req, res) {
-	var body = req.body;
+	var body = _.pick(req.body,'description','completed');
+	//req.body;
+
+	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length == 0) {
+		return res.status(400).send();
+	}
+
+	body.description = body.description.trim();
 	body.id = todoNextId;
 	todoNextId++;
 	todos.push(body);
